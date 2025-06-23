e087485f9b70592c4d79f6af569ea586c00ed9b5
Gean <geanvictorduque@gmail.com>
2025-06-23 18:21:20 -0300

    fix: fix datasource entities

--- src/config/datasource.ts
+++ src/config/datasource.ts
@@ -11,7 +11,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  entities: [__dirname + '/../**/*.entities{.ts,.js}'],
   migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
   synchronize: false,
 });
