1cf3306e274adea0ccc5e49503ad0d9078ff00ac
Gean <geanvictorduque@gmail.com>
2025-06-23 21:52:51 -0300

    fix: fix brevo module imports

--- src/modules/brevo/brevo.module.ts
+++ src/modules/brevo/brevo.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Brevo } from './entities/brevo.entity';
+import { User } from '../user/entities/user.entity';
 
 import { BrevoController } from './brevo.controller';
 import { BrevoService } from './brevo.service';
 import { BrevoRepository } from './brevo.repository';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Brevo, User])],
   controllers: [BrevoController],
   providers: [BrevoService, BrevoRepository],
 })
