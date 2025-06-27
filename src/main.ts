59e66b6717dc1117c697c604517330c60eff628c
Gean <geanvictorduque@gmail.com>
2025-06-26 23:49:24 -0300

    feat: add rabbitmq

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 import { AppModule } from './app.module';
 
@@ -9,6 +10,20 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amq://localhost:5672'],
+      queue: 'notifications_queue',
+      queueOptions: {
+        durable: true,
+        deadLetterExchange: '',
+        deadLetterRoutingKey: 'notification_dead_letter',
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
