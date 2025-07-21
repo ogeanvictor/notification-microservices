5ba5caa609a0458e532b26567ac890071735b0ec
Gean <geanvictorduque@gmail.com>
2025-07-21 18:11:23 -0300

    feat: create send template cloud api method

---
+++ src/modules/facebook/dtos/facebook-wpp.dto.ts
@@ -0,0 +1,44 @@
+import { IsNotEmpty } from 'class-validator';
+
+export class CurrencyParameter {
+  fallback_value: string;
+  code: string;
+  amount_1000: number;
+}
+
+export class DateTimeParameter {
+  fallback_value: string;
+  day_of_week: number;
+  year: number;
+  month: number;
+  day_of_month: number;
+  hour: number;
+  minute: number;
+  calendar: string;
+}
+
+export class TemplateComponentParameter {
+  type: string;
+  text?: string;
+  currency?: CurrencyParameter;
+  date_time?: DateTimeParameter;
+}
+
+export class TemplateComponent {
+  type: string;
+  parameters: TemplateComponentParameter;
+}
+
+export class FacebookWppDto {
+  @IsNotEmpty()
+  to: string;
+
+  @IsNotEmpty()
+  template: string;
+
+  @IsNotEmpty()
+  templateCode: string;
+
+  @IsNotEmpty()
+  components: TemplateComponent[];
+}
--- src/modules/facebook/facebook.service.ts
+++ src/modules/facebook/facebook.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 
@@ -9,6 +9,7 @@ import { decrypt, encrypt } from 'src/common/utils/cryptKey';
 
 import { FacebookCreateDto } from './dtos/facebook-create.dto';
 import { FacebookTemplatesDto } from './dtos/facebook-templates.dto';
+import { FacebookWppDto } from './dtos/facebook-wpp.dto';
 
 @Injectable()
 export class FacebookService {
@@ -34,11 +35,59 @@ export class FacebookService {
     try {
       const facebook: Facebook = await this.repository.findByUser(userId);
 
+      if (!facebook) {
+        throw new NotFoundException(
+          'Facebook Cloud API account for this user is not found, please create a account!',
+        );
+      }
+
       const decryptToken = decrypt(facebook.token);
 
       const response = await firstValueFrom(
         this.httpService.get<FacebookTemplatesDto>(
           `https://graph.facebook.com/v22.0/${facebook.businessPhone}/message_templates`,
+          {
+            headers: {
+              Authorization: `Bearer ${decryptToken}`,
+            },
+          },
+        ),
+      );
+
+      return response.data;
+    } catch (error: any) {
+      throw error;
+    }
+  }
+
+  async sendTemplate(body: FacebookWppDto, userId: string): Promise<any> {
+    try {
+      const facebook: Facebook = await this.repository.findByUser(userId);
+
+      if (!facebook) {
+        throw new NotFoundException(
+          'Facebook Cloud API account for this user is not found, please create a account!',
+        );
+      }
+
+      const decryptToken = decrypt(facebook.token);
+
+      const response = await firstValueFrom(
+        this.httpService.post(
+          `https://graph.facebook.com/v22.0/${facebook.businessPhone}/messages`,
+          {
+            messaging_product: 'whatsapp',
+            recipient_type: 'individual',
+            to: body.to,
+            type: 'template',
+            template: {
+              name: body.template,
+              language: {
+                code: body.templateCode,
+              },
+              components: body.components,
+            },
+          },
           {
             headers: {
               Authorization: `Bearer ${decryptToken}`,
