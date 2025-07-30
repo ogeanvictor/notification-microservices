4ed6005c71229cd892ed9beb97072a21e69ab0fa
Gean <geanvictorduque@gmail.com>
2025-07-29 23:34:40 -0300

    fix: fix express type

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,9 +1,13 @@
-declare module 'express' {
-  interface Request {
-    headers: any;
-    user?: {
-      id: string;
-      email: string;
-    };
+import * as express from 'express';
+
+declare global {
+  namespace Express {
+    interface Request {
+      headers: any;
+      user?: {
+        id: string;
+        email: string;
+      };
+    }
   }
 }
